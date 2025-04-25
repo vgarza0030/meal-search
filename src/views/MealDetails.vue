bf3b2a71f1b2cfbe36e74e5a3a4b9a9d48aca32d
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-04-25 17:12:07 -0500

    Changed order

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -18,7 +18,18 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="">
-        {{ meal }}
+    <div class="m-10">
+       <!--{{ meal }} -->
+        <h1 class="text-5xl font-bold mb-10"> {{ meal.strMeal }}</h1>
+        <img :src="meal.strMealThumb" class="max-w-[25%] mb-10" />
+        <p class="mb-10"> {{ meal.streInstructions }}</p>
+
+        <ul>
+             <li v-for="i in 20" :key="i">
+                <div v-if="meal['strIngredients' + i] !== ''">
+                {{ i }}. {{ ['strIngredients' + i] }}
+                </div>
+             </li>
+        </ul>
     </div>
 </template>
